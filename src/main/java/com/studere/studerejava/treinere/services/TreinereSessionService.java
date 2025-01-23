0b49b1a7384a391507a324792fa4855b38bcafc9
Cácio Lucas <cclucas060901@gmail.com>
2025-01-23 01:52:59 -0300

    feat: remove sets for simplicity

--- src/main/java/com/studere/studerejava/treinere/services/TreinereSessionService.java
+++ src/main/java/com/studere/studerejava/treinere/services/TreinereSessionService.java
@@ -1,6 +1,5 @@
 package com.studere.studerejava.treinere.services;
 
-import com.studere.studerejava.framework.repositories.ModuleRepository;
 import com.studere.studerejava.framework.repositories.SessionRepository;
 import com.studere.studerejava.framework.services.PlanService;
 import com.studere.studerejava.framework.services.SessionService;
@@ -9,19 +8,16 @@ import com.studere.studerejava.treinere.models.TreinereRoutine;
 import com.studere.studerejava.treinere.models.TreinereSession;
 
 public class TreinereSessionService extends SessionService<TreinereSession> {
-  private final SetService setService;
 
-  public TreinereSessionService(
-    SessionRepository<TreinereSession> sessionRepository,
-    PlanService<TreinereRoutine, Exercise> planService,
-    SetService setService
-  ) {
-    super(sessionRepository, planService);
-    this.setService = setService;
-  }
+    public TreinereSessionService(
+            SessionRepository<TreinereSession> sessionRepository,
+            PlanService<TreinereRoutine, Exercise> planService
+    ) {
+        super(sessionRepository, planService);
+    }
 
-  @Override
-  protected TreinereSession createNewSession() {
-    return new TreinereSession();
-  }
+    @Override
+    protected TreinereSession createNewSession() {
+        return new TreinereSession();
+    }
 }
